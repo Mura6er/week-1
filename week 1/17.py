382d312d55cc40bb1b10db6e67f61ff7e9523b90
Murager Userbay <usmurager@mail.ru>
2021-02-01 22:12:30 +0600

    qwer

---
+++ week 1/17.py
@@ -0,0 +1,17 @@
+# cars = ["Ford", "Volvo", "BMW"]
+
+# cars[0] = "Toyota"
+
+# print(cars)
+
+# cars = ["Ford", "Volvo", "BMW"]
+
+# x = len(cars)
+
+# print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+
+for x in cars:
+  print(x)
+
